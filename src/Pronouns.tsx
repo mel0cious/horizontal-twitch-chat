c075d5f8d1e0815b474948dd783d15a331487c6f
Vert <73512586+VertUnderscore@users.noreply.github.com>
2026-08-22 16:46:34 -0400

    added pronouns support

--- src/Pronouns.tsx
+++ src/Pronouns.tsx
@@ -4,35 +4,70 @@ interface PronounsProps {
     chat_user : string
 }
 
-function getDisplayPronouns(response) {
-    
+interface PronounsAPI {
+    channel_id: string,
+    channel_login: string,
+    pronoun_id: string,
+    alt_pronoun_id: string | null | undefined // if no alt pronouns
+}
+
+// Taken from https://api.pronouns.alejo.io/api/pronouns as of 8/22/2026
+const pronounsDisplayDict: {[key: string]: string} = {
+    "hehim": "He/Him",
+    "sheher": "She/Her",
+    "theythem": "They/Them",
+    "any": "Any/Any",
+    "other": "Other/Other",
+    "xexem": "Xe/Xem",
+    "faefaer": "Fae/Faer",
+    "vever": "Ve/Ver",
+    "aeaer": "Ae/Aer",
+    "ziehir": "Zie/Hir",
+    "perper": "Per/Per",
+    "eem": "E/Em",
+    "itits": "It/Its"
+}
+
+function parsePronouns(pronouns : (PronounsAPI | null)) : (string | undefined | null) {
+    if (pronouns === null) return null
+    else {
+        if (!pronouns.alt_pronoun_id) return pronounsDisplayDict[pronouns.pronoun_id]
+        
+        const primaryPronouns = pronounsDisplayDict[pronouns.pronoun_id].split("/")
+        const secondaryPronouns = pronounsDisplayDict[pronouns.alt_pronoun_id].split("/")
+        return `${primaryPronouns[0]}/${secondaryPronouns[0]}`
+    }
 }
 
 export default function Pronouns({chat_user} : PronounsProps) {
-    const [pronouns, setPronouns] = useState<string>("")
+    const [pronouns, setPronouns] = useState<PronounsAPI | null>(null)
 
-    useEffect(() => {
-        async function fetchPronouns() {
+    async function fetchPronouns(chat_user : string) : Promise<void>{
         const pronouns = await fetch(`https://api.pronouns.alejo.io/v1/users/${chat_user}`)
 
         if (pronouns.status == 404) {
-            // Pass
-        }
+                // Pass
+            }
         else {
             const pronounsJSON = pronouns.json()
-            pronounsJSON.then((response)=> {
-                console.log(response)
+            pronounsJSON.then((response:unknown)=> {
+                setPronouns(response as PronounsAPI)
+
             })
         }
     }
-
-    fetchPronouns()
+    useEffect(() => {
+        fetchPronouns(chat_user)
     }, [])
 
     
 
     return (
         <>
+        {(parsePronouns(pronouns))
+        ? <div className="pronouns">({parsePronouns(pronouns)})</div>
+        : <></>
+        }
         </>
     )
 }
