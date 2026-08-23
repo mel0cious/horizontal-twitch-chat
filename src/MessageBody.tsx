f7a512d14eb1501e1afb47e0a54d0ed1fa5ee70e
mel0cious <melodiewav@gmail.com>
2026-08-22 23:45:48 -0400

    sanitized user input, prevents xss attacks

--- src/MessageBody.tsx
+++ src/MessageBody.tsx
@@ -6,8 +6,28 @@ interface MessageBodyProps {
     chat_message: CompressedChatMessage
 }
 
+// For the function sanitize:
+// Source - https://stackoverflow.com/a/48226843
+// Posted by SilentImp, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-08-22, License - CC BY-SA 4.0
+
+function sanitize(string) {
+  const map = {
+      '&': '&amp;',
+      '<': '&lt;',
+      '>': '&gt;',
+      '"': '&quot;',
+      "'": '&#x27;',
+      "/": '&#x2F;',
+  };
+  const reg = /[&<>"'/]/ig;
+  return string.replace(reg, (match)=>(map[match]));
+}
+
+
 export default function MessageBody({chat_message} : MessageBodyProps) {
-    const parsed = emoteParser.parse(chat_message.text);
+    const sanitized = sanitize(chat_message.text)
+    const parsed = emoteParser.parse(sanitized);
 
     console.log(parsed)
     return (
